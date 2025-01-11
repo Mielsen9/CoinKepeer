963652b7aa124586dec70829c7eec05639d9007a
Petro <worldinthegame@gmail.com>
2025-01-11 10:31:03 +0200

    SpendMoney.module.scss SpendMoney.tsx

---
+++ src/components/SpendMoney/SpendMoney.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as s from "./SpendMoney.module.scss";
+
+
+
+type SpendMoneyProps = {};
+export const SpendMoney = (props: SpendMoneyProps) => {
+
+	return (
+		<div className={s.spendMoney}>
+
+		</div>
+	)
+};
