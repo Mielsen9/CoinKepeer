a4f9235f52b84d61c4ef3dbe6864a2856f820147
Petro <worldinthegame@gmail.com>
2025-01-11 10:33:59 +0200

    check point

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import {SavedMoneyArea} from "@/components/SavedMoneyArea/SavedMoneyArea";
+import {SpendMoneyArea} from "@/components/SpendMoneyArea/SpendMoneyArea";
+import {ExpensesButton} from "@/components/ExpensesButton/ExpensesButton";
 
 
 // Type
@@ -12,7 +15,14 @@ export const App = () => {
     // Return
     return (
         <div>
+            <SavedMoneyArea/>
 
+            <SpendMoneyArea/>
+            <SpendMoneyArea/>
+            <SpendMoneyArea/>
+            <SpendMoneyArea/>
+
+            <ExpensesButton/>
         </div>
     );
 };
