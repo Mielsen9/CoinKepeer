65f5965358edbf7734951a2fd07d455f7ed9c10f
Petro <worldinthegame@gmail.com>
2025-01-11 10:33:13 +0200

    ExpensesButton.module.scss ExpensesButton.tsx

---
+++ src/components/ExpensesButton/ExpensesButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as s from "./ExpensesButton.module.scss";
+
+type ExpensesButtonProps = {
+
+};
+export const ExpensesButton = (props: ExpensesButtonProps) => {
+
+	return (
+		<div className={s.expensesButton}>
+
+		</div>
+	)
+};
