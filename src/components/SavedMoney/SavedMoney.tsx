69821608381029819facaf7694a26d2341942031
Petro <worldinthegame@gmail.com>
2025-01-11 10:27:08 +0200

    SavedMoney.module.scss SavedMoney.tsx

---
+++ src/components/SavedMoney/SavedMoney.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as s from "./SavedMoney.module.scss";
+
+type SavedMoneyProps = {
+
+};
+export const SavedMoney = (props: SavedMoneyProps) => {
+
+	return (
+		<div className={s.savedMoney}>
+
+		</div>
+	)
+};
